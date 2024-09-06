a95c85a58bb3fb512647066d52633c53963186ab
khurshid <shamsun.com@gmail.com>
2024-09-06 16:47:16 +0500

    Firebase settings done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instaclone/pages/signin_page.dart';
 import 'package:instaclone/pages/signup_page.dart';
@@ -5,7 +6,18 @@ import 'package:instaclone/pages/splash_page.dart';
 
 import 'pages/home_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyCwIix_lK4NnoULOH6PYkxxCuBWcTIRKKs',
+      appId: '1:562947487290:android:306582f20b1759c9cd2703',
+      messagingSenderId: '562947487290',
+      projectId: 'instaclone-2-e33a7',
+    ),
+  );
+
+
   runApp(const MyApp());
 }
 
